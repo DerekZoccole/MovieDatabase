4b5ced7f05d123523ccea8f229d944ea44a25bdb
Derek Z <dzoccole@lakeheadu.ca>
2017-11-13 21:48:16 -0500

    Added MovieDatabase detail view

--- MovieDatabase/Controllers/MovieDatabaseController.cs
+++ MovieDatabase/Controllers/MovieDatabaseController.cs
@@ -10,12 +10,7 @@ namespace MovieDatabase.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("From the movie database controller");
+            return View();
         }
     }
 }
